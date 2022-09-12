2699a7c3f8f36fb159806f4a4d5e3e8f8f4055ca
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-09-12 18:18:35 +0530

    Time: 43 ms (7.62%), Space: 14.3 MB (57.06%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,32 +1,34 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* f(TreeNode* root,TreeNode* p,TreeNode* q){
-        
-        if(root == NULL || root == p || root == q) return root;
-        
-        TreeNode* l = f(root->left,p,q);
-        
-        TreeNode* m = f(root->right,p,q);
-        
-        if(l == NULL) return m;
-        else if(m == NULL) return l;
-        else{
+class Solution
+{
+    public:
+        TreeNode* f(TreeNode *root, TreeNode *p, TreeNode *q)
+        {
+
+            if (root == NULL || root == p || root == q) return root;
+
+            TreeNode *l = f(root->left, p, q);
+
+            TreeNode *m = f(root->right, p, q);
+            
+            
+            if (l == NULL) return m;
+            
+            else if (m == NULL) return l;
+            
             return root;
         }
-        return root;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        return f(root,p,q);
+    TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
 
+        return f(root, p, q);
     }
 };

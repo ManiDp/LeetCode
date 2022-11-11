35ea00b0adfc1d482949ce77daa42c503b9bcfb7
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-11-11 12:10:20 +0530

    Time: 6 ms (98.53%), Space: 7 MB (75.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -14,25 +14,20 @@ public:
     }
     string longestPalindrome(string s) {
         
-        int n = s.size(),i1=0,j1=0,lps=1 ;
         
-        if(n == 1 || n == 0) return s;
-        
-        for(int i=0;i<n;i++){
-            for(int j=n-1;j>i ;--j){
-                
-                bool palstr = false;
-                
-                if(lps < j-i+1)  palstr = palindrome(i,j,s);
-                
-                if(palstr && (j-i)+1 > lps){
-                    i1 = i;
-                    j1 = j;
-                    lps = j1-i1+1;
-                }
-            }
-        }
-        
-        return s.substr(i1,lps);
+    if (s.empty()) return "";
+    if (s.size() == 1) return s;
+    int min_start = 0, max_len = 1;
+    for (int i = 0; i < s.size();) {
+      if (s.size() - i <= max_len / 2) break;
+      int j = i, k = i;
+      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+      i = k+1;
+      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+      int new_len = k - j + 1;
+      if (new_len > max_len) { min_start = j; max_len = new_len; }
+    }
+    return s.substr(min_start, max_len);
+
     }
 };

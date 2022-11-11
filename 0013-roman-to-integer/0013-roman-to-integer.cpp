8e49c245799dabd2eda88ff6d2954b59e5be259a
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-11-11 12:45:24 +0530

    Time: 40 ms (20.25%), Space: 7.9 MB (46.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     int romanToInt(string s) {
         
-        unordered_map<char,int>ump;
-        
-        ump['I'] = 1;
-        ump['V'] = 5;
-        ump['X'] = 10;
-        ump['L'] = 50;
-        ump['C'] = 100;
-        ump['D'] = 500;
-        ump['M'] = 1000;
-        
+        unordered_map<char,int>ump{
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };        
         int n=s.size(),total = 0,prev = 0;
         
-        total +=ump[s[0]];
-        prev = ump[s[0]];
-        
-        for(int i=1;i<n;++i){
+        for(int i=0;i<n-1;++i){
             
-            total +=ump[s[i]];
-            
-            if(prev < ump[s[i]]){
-                total -= 2*prev;
-            }
-            
-            prev = ump[s[i]];
+            if(ump[s[i]] < ump[s[i+1]]) total -=ump[s[i]];
+            else total +=ump[s[i]];
         }
         
+        total +=ump[s[n-1]];
         return total;
     }
 };

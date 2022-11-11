1eff272b999fa08c31acae18c7ae467df862cbc8
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-11-11 11:20:33 +0530

    Time: 1695 ms (5.00%), Space: 6.9 MB (75.83%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool palindrome(int i,int j,string& s){
+        
+        int i1 = i;
+        
+        int l = j-i+1;
+        
+        for(i;i<i1+(l/2);++i){
+                if(s[i] != s[j] ) return false;
+                j--;
+            }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        
+        int n = s.size(),i1=0,j1=0,lps=1 ;
+        
+        if(n == 1 || n == 0) return s;
+        
+        for(int i=0;i<n;i++){
+            for(int j=n-1;j>i ;--j){
+                
+                bool palstr = palindrome(i,j,s);
+                
+                if(palstr && (j-i)+1 > lps){
+                    i1 = i;
+                    j1 = j;
+                    lps = j1-i1+1;
+                }
+            }
+        }
+        cout<<i1<<" "<<j1<<endl;
+        return s.substr(i1,lps);
+    }
+};

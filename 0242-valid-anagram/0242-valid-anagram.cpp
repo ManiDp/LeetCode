50ccba426fa0e2a437e0a97f2aee073a5b6e34cd
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-11-12 22:24:22 +0530

    Time: 13 ms (79.90%), Space: 7.4 MB (13.10%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.size() != t.size()) return false;
+        
+        vector<int>check(26,0);
+        int n=s.size();
+        for(int i=0;i<n;++i){
+            
+            check[s[i] - 'a']++;
+            check[t[i] - 'a']--;
+        }
+        
+        for(int i=0;i<26;++i){
+            if(check[i]) return false;
+        }
+        return true;
     }
 };

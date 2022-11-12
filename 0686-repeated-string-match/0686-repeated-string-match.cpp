1ef82a8f384c825398247753ee60f6fc113c2955
Mani Deep <63196422+ManiDp@users.noreply.github.com>
2022-11-12 16:11:11 +0530

    Time: 10 ms (76.23%), Space: 7.3 MB (40.02%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,56 +1,62 @@
 class Solution {
+    int BASE = 1e6;
 public:
+    int rabin_karp(string source,string target){
+        
+        int power=1,targetcode = 0,hashcode = 0,m = target.size(),n=source.size();
+        
+        for(int i=0;i<m;++i){
+            
+            power = (power*31)%BASE;
+        }
+        
+        for(int i=0;i<m;++i){
+            
+            targetcode = (targetcode*31 + target[i])%BASE;
+        }
+        cout<<targetcode<<endl;
+        
+        for(int i=0;i<n;++i){
+            
+            hashcode = (hashcode*31 + source[i])%BASE;
+            
+            if(i<m-1) continue;
+            
+            if(i>=m){
+                hashcode = (hashcode - (source[i-(m)]*power))%BASE;
+            }
+            if(hashcode<0)
+                hashcode+=BASE;
+            
+            if(hashcode == targetcode){
+                if(source.substr(i-(m-1),m) == target) return i-(m-1);
+            }
+            
+            // hashcode = (hashcode - (source[i-(m-1)]*power))%BASE;
+            // if(hashcode<0)
+            //     hashcode+=BASE;
+        }
+        
+        return -1;
+    }
     int repeatedStringMatch(string a, string b) {
         
-        int n=a.size(), m = b.size();
+        if(a == b) return 1;
         
-        int l = m/n ;
+        int count = 0;
         
-        string temp = "",temp2 = a;
+        string str = "";
         
-        if(m%n != 0) l++;
-        
-        for(int i=0;i<l;++i) temp +=a;
-        
-        a = temp;
-        
-        n = a.size();
-        
-        for(int i=0;i<n;++i){
+        while(str.size() < b.size()){
             
-            if(a[i] == b[0]){
-                
-                int j = i,k = i;
-                
-                for(j;j<k+m;++j){
-                    
-                    if(a[j] != b[j-k]) break;
-                }
-                
-                if( j == k+m) return l;
-                
-            }
+            str  +=a;
+            count++;
         }
+        if(str == b) return count;
         
-        a +=temp2;
+        if(rabin_karp(str,b) != -1) return count;
+        if(rabin_karp(str+a,b) != -1) return count+1;
         
-        n = a.size();
-        
-        for(int i=0;i<n;++i){
-            
-            if(a[i] == b[0]){
-                
-                int j = i,k = i;
-                
-                for(j;j<k+m;++j){
-                    
-                    if(a[j] != b[j-k]) break;
-                }
-                
-                if( j == k+m) return l+1;
-                
-            }
-        }
         return -1;
     }
 };
